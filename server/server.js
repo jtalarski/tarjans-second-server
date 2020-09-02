cda99e789546dd7d827af39f570fc82d02ddbb2f
Emma Westbrook <emmalwestbrook@gmail.com>
2020-09-02 10:53:09 -0500

    node server boilerplate

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
+const express = require('express');
+
 console.log("Server time");
+
+const app = express();
+
+
+// listening usually goes at the end, since you have to
+// wait for app to be created
+const port = 3000;
+app.listen(port, function () {
+    console.log("listening at 3000");
+});
