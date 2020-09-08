778df37aa2dc549912530c15fdfe1cdc9a3a80a5
Jim Talarski <jim.talarski@gmail.com>
2020-09-08 13:35:59 -0500

    spelling fixed

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const activitiesRouter = require('./activites')
+const activitiesRouter = require('./activities')
 console.log("Look at me, I'm a server!")
 
 // Create an express app
